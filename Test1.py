9c43878bcd2449fd69e6264e5450045a9b6be359
Giovani Rossi <gzr0031@auburn.edu>
2020-03-12 00:41:06 -0500

    Double push?

--- Test1.py
+++ Test1.py
@@ -1,4 +1,5 @@
 print("Hello Everyone")
 print("Let's make some change and see how it will works")
 print("Another change")
-Print("one more time")
+print("one more time")
+print("another change before pushing")
